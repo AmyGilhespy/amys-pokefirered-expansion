66f8a8ae9f51b094ceebc5ec84059b810f286f76
Amy Gilhespy <amyogilhespy@gmail.com>
2025-10-31 07:10:59 -0500

    Tuned the pokemon center a bit.

--- src/field_effect.c
+++ src/field_effect.c
@@ -866,7 +866,7 @@ static void PokeballGlowEffect_PlaceBalls(struct Sprite *sprite)
     u8 spriteId;
     if (sprite->sTimer == 0 || (--sprite->sTimer) == 0)
     {
-        sprite->sTimer = 25;
+        sprite->sTimer = 10;
         spriteId = CreateSpriteAtEnd(&sSpriteTemplate_PokeballGlow, sPokeballCoordOffsets[sprite->sCounter].x + sprite->x2, sPokeballCoordOffsets[sprite->sCounter].y + sprite->y2, 0xFF);
         gSprites[spriteId].oam.priority = 2;
         gSprites[spriteId].sGlowEffectSpriteId = sprite->sSpriteId;
